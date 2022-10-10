9ee5437f865e340783113754a719f8efcd7409c8
Enzo ARCHAMBAUD <bib.enzoarchambaud@gmail.com>
2022-10-10 11:09:16 +0200

    modification serie + ajoue secu

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -16,15 +16,22 @@ class ApiController extends AbstractController
     public function getListeSeries(PdoFouDeSerie $pdoFouDeSerie)
     {
         $listeSeries = $pdoFouDeSerie->getLesSeries();
-        $tableauSeries=[];
-        foreach($listeSeries as $uneSerie)
+        if($listeSeries!=null)
         {
-            $tableauSeries[]=['id'=>$uneSerie['id'],'titre'=>$uneSerie['titre'],'resume'=>$uneSerie['resume'],'duree'=>$uneSerie['duree'],'premiereDiffusion'=>$uneSerie['premiereDiffusion'],'image'=>$uneSerie['image'],];
+            $tableauSeries=[];
+            foreach($listeSeries as $uneSerie)
+            {
+                $tableauSeries[]=['id'=>$uneSerie['id'],'titre'=>$uneSerie['titre'],'resume'=>$uneSerie['resume'],'duree'=>$uneSerie['duree'],'premiereDiffusion'=>$uneSerie['premiereDiffusion'],'image'=>$uneSerie['image'],];
+            }
+            return new JsonResponse($tableauSeries);
         }
-        return new JsonResponse($tableauSeries);
+        else
+        {
+            return new JsonResponse(['message'=>'Serie not found'], Response::HTTP_NOT_FOUND);
+        }
     }
 
-    #[Route('/api/series/{id}', name:'app_apiUneSerie')]
+    #[Route('/api/series/{id}', name:'app_apiUneSerie',methods:['GET'])]
     public function getLaSerie(PdoFouDeSerie $pdoFouDeSerie,$id)
     {
         $laSerie = $pdoFouDeSerie->getLaSerie($id);
@@ -43,7 +50,36 @@ class ApiController extends AbstractController
     #[Route('/api/series', name:'app_addUneSerie',methods:['POST'])]
     public function newSerie(Request $request, PdoFouDeSerie $pdoFouDeSerie)
     {
-        $pdoFouDeSerie->setLaSerie(json_decode($request->getContent(),true));
-        return new JsonResponse(['message'=>'Serie a été ajouté'], Response::HTTP_CREATED);
+        $reponse = $pdoFouDeSerie->setLaSerie(json_decode($request->getContent(),true));
+        return new JsonResponse(['message'=>'Serie a été ajouté','id'=>$reponse['id'] ,'titre'=>$reponse['titre'], 'resume'=>$reponse['resume'], 'duree'=>$reponse['duree'], 'premiereDiffusion'=>$reponse['premiereDiffusion'], 'image'=>$reponse['image']], Response::HTTP_CREATED);
+    }
+
+    #[Route('/api/series/{id}', name:'app_deleteUneSerie',methods:['DELETE'])]
+    public function deleteSerie($id, PdoFouDeSerie $pdoFouDeSerie)
+    {
+        if($pdoFouDeSerie->getLaSerie($id)!=null)
+        {
+            $pdoFouDeSerie->deleteSerie($id);
+            return new JsonResponse(['message'=>'Serie as deleted'], Response::HTTP_ACCEPTED);
+        }
+        else
+        {
+            return new JsonResponse(['message'=>'Serie not found'], Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    #[Route('/api/series/{id}', name:'app_editUneSerie',methods:['PUT'])]
+    public function editSerie($id, PdoFouDeSerie $pdoFouDeSerie, Request $request)
+    {
+        if($pdoFouDeSerie->getLaSerie($id)!=null)
+        {
+            $laSerie = $pdoFouDeSerie->getLaSerie($id);
+            $pdoFouDeSerie->updateSerieComplete($laSerie, $id, json_decode($request->getContent(),true));
+            return new JsonResponse(['message'=>'Serie as modified'], Response::HTTP_ACCEPTED);
+        }
+        else
+        {
+            return new JsonResponse(['message'=>'Serie not found'], Response::HTTP_NOT_FOUND);
+        }
     }
 }
