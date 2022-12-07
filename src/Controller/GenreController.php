1ecb3b65dd67c0941221c8489228cefab45b575d
Enzo ARCHAMBAUD <bib.enzoarchambaud@gmail.com>
2022-12-07 08:11:01 +0100

    fndfjkzsnvju

--- src/Controller/GenreController.php
+++ src/Controller/GenreController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Genre;
+use App\Entity\Serie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class GenreController extends AbstractController
 {
-    #[Route('/genre', name: 'app_genre')]
-    public function testGenre(): Response
+    #[Route('/testGenre/{id}', name: 'app_testGenre')]
+    public function testGenre(ManagerRegistry $doctrine,$id)
     {
-        return $this->render('genre/index.html.twig', [
-            'controller_name' => 'GenreController',
-        ]);
+        $genre = new Genre();
+        if($doctrine->getRepository(Genre::class)->findBy(["libelle" => $genre->getLibelle('Drame')])!= null)
+        {
+            $genre->setLibelle('Drame');
+            $entityManager = $doctrine->getManager();
+            $serie = $doctrine->getRepository(Serie::class)->find($id);
+            $serie->addGenre($genre);
+            $entityManager->persist($genre);
+            $entityManager->flush();
+            return $this->render('genre/addGenre.html.twig',['serie'=>$serie]);
+        }
+        $entityManager = $doctrine->getManager();
+        $serie = $doctrine->getRepository(Serie::class)->find($id);
+        $serie->addGenre($genre);
+        $entityManager->persist($genre);
+        $entityManager->flush();
+        return $this->render('genre/addGenre.html.twig',['serie'=>$serie]);
     }
 }
