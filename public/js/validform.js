143ffd66ba8ea66b0b135d6e84b89fb93aabc578
Enzo ARCHAMBAUD <bib.enzoarchambaud@gmail.com>
2023-03-03 17:03:14 +0100

    disable button not hide

--- public/js/validform.js
+++ public/js/validform.js
@@ -4,9 +4,9 @@ button = document.getElementById("add");
 image.addEventListener("change", function (e){
     if(e.target.value.search('\.(png|jpg)$') != -1){0
         erreur.innerHTML='';
-        button.style.display = "";
+        button.disabled = false
     } else {
         erreur.innerHTML='Votre saisie est invalide';
-        button.style.display = "none";
+        button.disabled = true
     }
 });
