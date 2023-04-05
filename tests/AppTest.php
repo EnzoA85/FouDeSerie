c808022ea219b3cdce5f2bfd052d41ae488bd786
Enzo ARCHAMBAUD <bib.enzoarchambaud@gmail.com>
2023-04-05 08:17:00 +0200

    add AppTest.php

---
+++ tests/AppTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\Tests;
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase
+{
+    public function testAreWorkingTests()
+    {
+        $this->assertEquals(2,1+5);
+    }
+}
